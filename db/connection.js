a588e99f896050781bb912b2f1d4e2d6bb6ada43
Lukesudom <lukemarshall511@gmail.com>
2022-08-03 20:00:04 +1000

    add remove employee and manager function

--- db/connection.js
+++ db/connection.js
@@ -2,30 +2,34 @@ import mysql from 'mysql2';
 
 import inquirer from "inquirer";
 
-import cTable from "console.table";
-import { ftruncateSync } from 'fs';
-import { consumers } from 'stream';
+// import cTable from "console.table";
+// import { ftruncateSync } from 'fs';
+// import { consumers } from 'stream';
+// import { connect } from 'http2';
+// import { type } from 'os';
 
 
 //VARIABLES -- 
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 8080,
-    user: "root",
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 8080,
+//     user: "root",
     
-    password:"",
-    database:"employees_db",
+//     password:"",
+//     database:"employees_db",
 
-});
+// });
 
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log ("you are connected as id:" + connection.threadId + "\n");
+// connection.connect(function(err) {
+//     if (err) throw err;
+//     console.log ("you are connected as id:" + connection.threadId + "\n");
 
-    performSearch();
-});
+//     performSearch();
+// });
+
+performSearch();
 
 function performSearch() {
 
@@ -162,7 +166,7 @@ function addEmployees() {
 
     .then(function(answer) {
 
-        let newEmpMgr = " ";
+        var newEmpMgr = " ";
 
         let answerNEM = newEmployeeManager;
 
@@ -184,7 +188,7 @@ function addEmployees() {
         }
 
 
-        let query = connection.query(
+        var query = connection.query(
             "INSERT INTO employee SET ?",
 
 
@@ -203,5 +207,160 @@ function addEmployees() {
                 performSearch();
             }
         )
-    });
+    })
 }
+
+function updateEmployeeRole() {
+
+    let query = "SELECT employee.id, employee.first_name, employee.last_name, department.dept_name, employee_roles_id, roles.title ";
+    query += "FROM employee";
+    query += "INNER JOIN department ON employee.emp_dept = department.dept_name";
+    query += "INNER JOIN roles ON department.id = roles.department_id";
+
+    connection.query(query, function(err,results) {
+        if (err) throw err;
+
+        inquirer
+        .prompt ([
+            {
+                name: "choice",
+                type:"rawlist",
+                message:"Who's role would you like to update",
+                choices: function() {
+
+
+                    let choiceArray = [];
+                    for (let i = 1; i < results.length; i++) {
+                        let emp = "";
+                        employee = `${results[i].id} ${results[i].first_name} ${results[i].last_name} ${results[i].dept_name} ${results[i].roles_id} ${results[i].title}`
+                        choiceArray.push(employee)
+                    }
+
+                    return choiceArray;
+                }
+            },
+
+            {
+                name: "updateRole",
+                type:"list",
+                message: "What update would you like to apply to this role?",
+                choices: ['Salesperson', 'Software Engineer', 'Lead Engineer', 'Account Manager', 'Accountant', 'Sales Team Lead', 'Legal Team Lead', 'Lawyer']
+            }
+        ])
+
+        .then(function(answer) {
+            updateChosenRole(answer);
+            return answer;
+        })
+    })
+}
+
+
+
+function updateChosenRole(answer) {
+    
+    newManager = "";
+    newDepartment = "";
+    newRoleID = "";
+
+    var answerUR = answer.updateRole;
+
+    if (answerUR === 'Salesperson') {
+
+        newManager = "5";
+        newDepartment = "Sales";
+        newRoleID = "2";
+    }
+
+    if (answerUR === 'Software Engineer') {
+
+        newManager = "4";
+        newDepartment = "Engineering";
+        newRoleID = "3";
+    }
+
+    if (answerUR === 'Lead Engineer') {
+
+        newManager = "4";
+        newDepartment = "Engineering";
+        newRoleID = "4";
+    }
+    if (answerUR === 'Account Manager') {
+
+        newManager = "5";
+        newDepartment = "Sales";
+        newRoleID = "5";
+    }
+    if (answerUR === 'Accountant') {
+
+        newManager = "9";
+        newDepartment = "Finance";
+        newRoleID = "6";
+    }
+    if (answerUR === 'Sales Team Lead') {
+
+        newManager = "5";
+        newDepartment = "Sales";
+        newRoleID = "7";
+    }
+    if (answerUR === 'Legal Team Lead') {
+
+        newManager = "3";
+        newDepartment = "Law";
+        newRoleID = "8";
+    }
+    if (answerUR === 'Lawyer') {
+
+        newManager = "3";
+        newDepartment = "Law";
+        newRoleID = "9";
+    }
+let choiceStr = answer.choice.split(" ")
+console.log(answer);
+console.log (choiceStr[0]);
+
+
+connection.query(
+    "UPDATE employee SET ? WHERE ?",
+    [
+        {
+            roles_id: newRoleID,
+            emp_dept: newDepartment,
+            manager_id: newManager,
+        },
+        {
+
+        id: parseInt(choiceStr[0])
+
+        }
+    ],
+
+    function(err, res) {
+        if (err) throw err;
+        console.log(res.affectedRows + "you have UPDATED the Employee's Role successfully");
+
+        performSearch();
+    }
+  )
+}
+
+
+function removeEmployee() {
+
+    let query = "SELECT employee.id, employee.first_name, employee.last_name";
+    query += "FROM employee";
+    connection.query(query, function(err, results) {
+        if (err) throw err;
+        inquirer
+        .prompt([
+            {
+                name: "choice",
+                type:"rawlist",
+                message: "Which employee do you want to delete?",
+                choices: functio
+            }
+        ])
+    })
+}
+
+
