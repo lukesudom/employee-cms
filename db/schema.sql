3382fe7fb1ed0181a4328e316db88cfdf8c19a12
Lukesudom <lukemarshall511@gmail.com>
2022-08-09 18:43:28 +1000

    sql syntax fix

--- db/schema.sql
+++ db/schema.sql
@@ -5,12 +5,12 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
--- Create Tables --
+Create Tables 
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
     deptartment_name VARCHAR(30) NOT NUll,
-    PRIMARY KEY(id)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
@@ -18,8 +18,9 @@ CREATE TABLE roles (
    title VARCHAR(30),
    salary DECIMAL,
    department_id INT,
-   FOREIGN KEY (department_id) REFERENCES department(id),
-   PRIMARY KEY (id),
+   FOREIGN KEY (department_id) REFERENCES department (id)
+   PRIMARY KEY (id)
+   ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
@@ -30,13 +31,14 @@ CREATE TABLE employee (
     employee_department VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     manager_id INT,
-    FOREIGN KEY(manager_id) REFERENCES(employee_id),
-    FOREIGN KEY(roles_id) REFERENCES roles(id),
-    PRIMARY KEY(id)
+    FOREIGN KEY (manager_id) REFERENCES (employee_id)
+    FOREIGN KEY (roles_id) REFERENCES roles (id)
+    PRIMARY KEY (id)
+    ON DELETE SET NULL
 
 );
 
 CREATE TABLE manager (
-    id INT NOT NULL
+    id INT NOT NULL,
     mgr_name VARCHAR(30) NOT NULL
 );
